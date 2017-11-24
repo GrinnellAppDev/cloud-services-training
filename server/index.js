b82cd74793de34ca09a4c498c0d11130a2ac254f
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-11-24 17:50:02 -0600

    Switch to JavaScript object instead of json

--- server/index.js
+++ server/index.js
@@ -12,27 +12,25 @@ express()
 
 express()
   .get("/tasks", (request, response) => {
-    response.status(200).send(`
-      {
-        "items": [
-          {
-            "id": "sl2ei3hf3iw",
-            "title": "Collect underpants",
-            "completed": false
-          },
-          {
-            "id": "tl2ei323xze",
-            "title": "???",
-            "completed": false
-          },
-          {
-            "id": "uz2ei32cx7e",
-            "title": "Profit!",
-            "completed": false
-          }
-        ]
-      }
-    `)
+    response.status(200).send({
+      items: [
+        {
+          id: "sl2ei3hf3iw",
+          title: "Collect underpants",
+          completed: false
+        },
+        {
+          id: "tl2ei323xze",
+          title: "???",
+          completed: false
+        },
+        {
+          id: "uz2ei32cx7e",
+          title: "Profit!",
+          completed: false
+        }
+      ]
+    })
   })
 
   .listen(API_PORT, () => {
