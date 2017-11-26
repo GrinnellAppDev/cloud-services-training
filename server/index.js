935a002075214c9ba92f8a68773db0eefa3d8c6e
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-11-26 14:01:04 -0600

    Remove new from throws

--- server/index.js
+++ server/index.js
@@ -70,7 +70,7 @@ express()
       )
 
       if (!updateResult.result.ok) {
-        throw new Error("Couldn't update database")
+        throw Error("Couldn't update database")
       }
 
       response.sendStatus(204)
@@ -87,7 +87,7 @@ express()
       })
 
       if (!deleteResult.ok) {
-        throw new Error("Couldn't update database")
+        throw Error("Couldn't update database")
       }
 
       response.status(200).send({ item: deleteResult.value })
