10a3eef92462620f3127f3197e5414d0f41bc86b
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2017-12-27 11:09:45 -0800

    Simplify ascii compare a little

--- webapp/src/util.js
+++ webapp/src/util.js
@@ -5,7 +5,7 @@ export const asciiCompare = (a, b) => {
   let comparison = 0
 
   for (let i = 0; comparison === 0 && (i < a.length || i < b.length); i++) {
-    comparison = (a.charCodeAt(i) || -1) - (b.charCodeAt(i) || -1)
+    comparison = (a.charCodeAt(i) || 0) - (b.charCodeAt(i) || 0)
   }
 
   return comparison
