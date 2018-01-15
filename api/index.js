3cc7a440bf341d89487a44638d41473b3c80f5f7
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-01-15 13:20:22 -0800

    Remove wrapper object from response

--- api/index.js
+++ api/index.js
@@ -4,25 +4,23 @@ const PORT = 2000
 
 express()
   .get("/tasks", (request, response) => {
-    response.status(200).send({
-      items: [
-        {
-          id: "3l2ei3hf3iw",
-          title: "Collect underpants",
-          isComplete: false
-        },
-        {
-          id: "2l2ei323xze",
-          title: "???",
-          isComplete: false
-        },
-        {
-          id: "1z2ei32cx7e",
-          title: "Profit!",
-          isComplete: false
-        }
-      ]
-    })
+    response.status(200).send([
+      {
+        id: "3l2ei3hf3iw",
+        title: "Collect underpants",
+        isComplete: false
+      },
+      {
+        id: "2l2ei323xze",
+        title: "???",
+        isComplete: false
+      },
+      {
+        id: "1z2ei32cx7e",
+        title: "Profit!",
+        isComplete: false
+      }
+    ])
   })
 
   .listen(PORT, () => {
