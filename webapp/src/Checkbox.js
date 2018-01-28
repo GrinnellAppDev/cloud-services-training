5a231a0999c0e91c82a1eff5f0028e6d0e8bec19
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-01-27 20:22:11 -0600

    Switch checkbox into functional component

--- webapp/src/Checkbox.js
+++ webapp/src/Checkbox.js
@@ -1,29 +1,21 @@
 import React from "react"
 import "./Checkbox.css"
+import classnames from "classnames"
 
-export class Checkbox extends React.Component {
-  // state = { isFocused: false }
+export const Checkbox = ({ className, ...props }) => (
+  <label className={classnames("Checkbox", className)}>
+    <input {...props} className="Checkbox-input" type="checkbox" />
 
-  // onFocus = () => this.setState({ isFocused: true })
-  // onBlur = () => this.setState({ isFocused: false })
-
-  render() {
-    return (
-      <label className="Checkbox">
-        <input {...this.props} className="Checkbox-input" type="checkbox" />
-
-        <svg className="Checkbox-svg" viewBox="-10 -10 120 120">
-          <circle className="Checkbox-borderRing" cx="50" cy="50" r="50" />
-          <circle className="Checkbox-focusRing" cx="50" cy="50" r="50" />
-          <path
-            className="Checkbox-check"
-            d="M72 25L42 71 27 56l-4 4 20 20 34-52z"
-          />
-          <circle className="Checkbox-ripple" cx="50" cy="50" r="50" />
-        </svg>
-      </label>
-    )
-  }
-}
+    <svg className="Checkbox-svg" viewBox="-10 -10 120 120">
+      <circle className="Checkbox-borderRing" cx="50" cy="50" r="50" />
+      <circle className="Checkbox-focusRing" cx="50" cy="50" r="50" />
+      <path
+        className="Checkbox-check"
+        d="M72 25L42 71 27 56l-4 4 20 20 34-52z"
+      />
+      <circle className="Checkbox-ripple" cx="50" cy="50" r="50" />
+    </svg>
+  </label>
+)
 
 export default Checkbox
